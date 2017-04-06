e39a9c4ed1a8850e808a5d45d493f0133eb377c8
CHENWEI73 <2102657566@qq.com>
2017-04-06 20:40:58 +0800

    实现原创微博的布局

--- WeiBo/WeiBo/Classes/View(视图)/Home(首页)/StatusesCell/WBOriginalStatusView.swift
+++ WeiBo/WeiBo/Classes/View(视图)/Home(首页)/StatusesCell/WBOriginalStatusView.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class WBOriginalStatusView: UIView {
+    /// 用户头像
+    lazy var userIcon: UIImageView = UIImageView(imageName: "avatar_default_big")
+    
+    /// 用户的vip图标
+    lazy var vipIcon: UIImageView = UIImageView(imageName: "avatar_grassroot")
+    
+    /// 用户的昵称
+    lazy var userNameLable: UILabel = UILabel(title: "小王爱睡觉")
+    
+    /// 用户的皇冠的等级图标
+    lazy var levelIcon: UIImageView = UIImageView(imageName: "common_icon_membership_level6")
+    
+    /// 微博的来源
+    lazy var sourceLable: UILabel = UILabel(title: "新浪", titleColor: UIColor.lightGray, fontSize: 12)
+    /// 微博的发布时间
+    lazy var timeLable: UILabel = UILabel(title: "两小时前", titleColor: UIColor.lightGray, fontSize: 12)
+    
+    //添加一个微博正文的label
+    lazy var statusLabel: UILabel = UILabel(title: nil)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,23 +40,64 @@ class WBOriginalStatusView: UIView {
     }
     
     
-    //添加一个微博正文的label
-    lazy var statusLabel: UILabel = UILabel(title: nil)
-    
+
 }
 
 
 extension WBOriginalStatusView {
     
     func setupUI() {
-        
+        //1.添加子控件
+        addSubview(userIcon)
+        addSubview(vipIcon)
+        addSubview(userNameLable)
+        addSubview(levelIcon)
+        addSubview(sourceLable)
+        addSubview(timeLable)
         addSubview(statusLabel)
         
-        statusLabel.numberOfLines = 0
-        statusLabel.snp.makeConstraints { (make) in
-            make.left.right.top.bottom.equalTo(self)
+        //2. 自动布局(自上而下, 从左到右, 依次布局, 当子控件较多时, 添加一两个就要测试)
+        userIcon.snp.makeConstraints { (make) in
+            make.left.top.equalTo(self).offset(10)
+            make.size.equalTo(CGSize(width: 35, height: 35))
         }
         
+        vipIcon.snp.makeConstraints { (make) in
+            make.left.equalTo(userIcon.snp.right).offset(-10)
+            make.top.equalTo(userIcon.snp.bottom).offset(-10)
+            make.size.equalTo(CGSize(width: 15, height: 15))
+        }
+        
+        userNameLable.snp.makeConstraints { (make) in
+            make.left.equalTo(userIcon.snp.right).offset(10)
+            make.top.equalTo(userIcon)
+        }
+        
+        levelIcon.snp.makeConstraints { (make) in
+            make.left.equalTo(userNameLable.snp.right).offset(10)
+            make.bottom.equalTo(userNameLable).offset(-3)
+            make.size.equalTo(CGSize(width: 15, height: 15))
+        }
+        
+        timeLable.snp.makeConstraints { (make) in
+            make.left.equalTo(userIcon.snp.right).offset(10)
+            make.bottom.equalTo(userIcon)
+        }
+        
+        sourceLable.snp.makeConstraints { (make) in
+            make.left.equalTo(timeLable.snp.right).offset(10)
+            make.bottom.equalTo(userIcon)
+        }
+        
+        statusLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(userIcon.snp.bottom).offset(10)
+            make.left.equalTo(userIcon)
+            make.right.equalTo(self).offset(-10)
+            make.bottom.equalTo(self).offset(-10)
+        }
+        
+        
+        statusLabel.numberOfLines = 0
         //测试用的值:
         var string = "苍井波多哦哦哦, 感觉自己蒙蒙的;"
         let randomCount = arc4random() % 20 + 1 //获取1到20的随机数
