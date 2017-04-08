eb74ebad970fb88ece2e2c60428e2c550276a039
CHENWEI73 <2102657566@qq.com>
2017-04-08 21:12:07 +0800

    实现配图视图的布局

--- WeiBo/WeiBo/Classes/View(视图)/Home(首页)/StatusesCell/WBStatusPictureView.swift
+++ WeiBo/WeiBo/Classes/View(视图)/Home(首页)/StatusesCell/WBStatusPictureView.swift
@@ -28,6 +28,37 @@ extension WBStatusPictureView {
     
     func setupUI() {
         self.backgroundColor = UIColor.yellow
+        
+        //裁剪超出配图部分
+        self.clipsToBounds = true
+        
+        //imageView 的宽高
+        let imageWH = (screenWidh - 40) / 3
+        
+        //移动一次就要调整 imageview 的 frame(水平和垂直的距离)
+        let gap =  imageWH + 10
+        
+        let firstImageViewFrame = CGRect(x: 0, y: 0, width: imageWH, height: imageWH)
+        //创建9个 imageView
+        for i in 0..<9 {
+            //创建
+            let imageView = UIImageView()
+            imageView.backgroundColor = UIColor.red
+            
+            //计算行数和列数
+            let row = i / 3
+            let col = i % 3
+            
+            //计算 frame
+            imageView.frame = firstImageViewFrame.offsetBy(dx: CGFloat(row) * gap, dy: CGFloat(col) * gap)
+            
+            addSubview(imageView)
+        }
+        
+        
+        
+        
+        
     }
     
     
